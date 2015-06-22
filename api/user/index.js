5e6d47a98daae5c766ac7e1c2131de261f2d81e3
Joel <90joelmarquez@gmail.com>
2015-06-21 22:00:27 -0300

    Se agregó la documentación para la ruta /api/user/:id

--- api/user/index.js
+++ api/user/index.js
@@ -8,6 +8,29 @@
 var userRoutes = require('express').Router();
 var User = require('./user');
 
+/**
+ * @api {get} /api/user/:id Obtener Usuario
+ * @apiGroup Usuarios
+ *
+ * @apiHeader {String} Authorization Bearer token
+ *
+ * @apiParam {String} id Id del usuario
+ *
+ * @apiSuccess {Object} user Usuario
+ *
+ * @apiSuccessExample Respuesta valida
+ *     HTTP/1.1 200 OK
+ *
+ *     {
+ *        "user": {
+ *          "mainCircle":"558748787f0a76cc4ca02a35",
+ *          "email":"90joelmarquez@gmail.com",
+ *          "name":"Joel",
+ *          "_id":"558748767f0a76cc4ca02a34",
+ *          "__v":0
+ *        }
+ *     }
+ */
 userRoutes.get('/:id', function(req, res) {
 
   process.nextTick(function() {
