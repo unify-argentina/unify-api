b5a046d9e5bfdc8895e9358354e1e9a24473aaea
Joel <90joelmarquez@gmail.com>
2015-07-01 02:19:46 -0300

    Se cambió la forma en la que se valida el user_id en los requests

--- api/user/index.js
+++ api/user/index.js
@@ -7,6 +7,25 @@
 // requires
 var userRoutes = require('express').Router();
 var userController = require('./user.controller');
+var User = require('./user.model.js');
+
+// Esto lo que hace es verificar que cada vez que se envíe un user_id como parámetro en una ruta,
+// coincida con el user que está en el request, previamente validado con el Json Web Token
+userRoutes.param('user_id', function(req, res, next, userId) {
+  // Validamos nosql injection
+  if (typeof req.params.id === 'object') {
+    return res.status(401).send({ errors: [{ msg: "You're trying to send object data types" }] });
+  }
+
+  // Si el req.user, ya habiendo pasado por la verificación del token es el mismo
+  // que el del req.params.id, enviamos el user
+  if (req.user !== userId) {
+    return res.status(401).send({ errors: [{ msg: 'You are trying to find a different user' }]});
+  }
+  else {
+    next();
+  }
+});
 
 /**
  * @api {get} /api/user/:id Obtener Usuario
@@ -31,8 +50,8 @@ var userController = require('./user.controller');
  *        }
  *     }
  */
-userRoutes.get('/:id', userController.getUserById);
+userRoutes.get('/:user_id', userController.getUserById);
 
-userRoutes.use('/:id/circle', require('../circle'));
+userRoutes.use('/:user_id/circle', require('../circle'));
 
 module.exports = userRoutes;
--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -10,29 +10,16 @@ var User = require('./user.model.js');
 module.exports.getUserById = function(req, res) {
 
   process.nextTick(function() {
-
-    // Nos aseguramos de que no nos manden objetos en el request
-    if (typeof req.params.id === 'object') {
-      return res.status(401).send({ errors: [{ msg: "You're trying to send object data types" }] });
-    }
-
-    // Si el req.user, ya habiendo pasado por la verificación del token es el mismo
-    // que el del req.params.id, enviamos el user
-    if (req.user === req.params.id) {
-      User
-        .findOne({ _id: req.params.id })
-        .populate('mainCircle')
-        .exec(function(err, user) {
+    User
+      .findOne({ _id: req.params.user_id })
+      .populate('mainCircle')
+      .exec(function(err, user) {
         if (err) {
-          res.status(401).send({ errors: [{ msg: 'Error finding user with id ' + req.params.id }] });
+          return res.status(401).send({ errors: [{ msg: 'Error finding user with id ' + req.params.id }] });
         }
         else {
-          res.status(200).send({ user: user });
+          return res.status(200).send({ user: user });
         }
       });
-    }
-    else {
-      res.status(401).send({ errors: [{ msg: 'You are trying to find a different user' }]});
-    }
   });
 };
