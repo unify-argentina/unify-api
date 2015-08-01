f2d34847e56b13d19c9b8a57a56da49b2018118e
Juan Cistaro <juan.cistaro@graion.com>
2015-08-01 20:58:21 -0300

    Se retira la etiqueta OPTIONS de los Method para CORS y se deja la validacion devolviendo status 200

--- config/express.js
+++ config/express.js
@@ -17,7 +17,7 @@ var allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:9000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
   // intercept OPTIONS method
   if ('OPTIONS' === req.method) {
