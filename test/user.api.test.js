729c4e03030d56436f0ba9329b15d4852cc27c4c
Joel <90joelmarquez@gmail.com>
2015-08-24 23:35:04 -0300

    Fix de los tests de usuario

--- test/user.api.test.js
+++ test/user.api.test.js
@@ -8,6 +8,7 @@
 var request = require('supertest');
 var mongoose = require('mongoose');
 var config = require('../config');
+var logger = require('../config/logger');
 
 // modelos
 var User = require('../api/user/user.model');
@@ -19,20 +20,20 @@ var USERS_PATH = '/api/user/';
 var LOGIN_PATH = '/auth/login';
 
 // Esta función sirve para hacer un login y devolverle al callback el user_id y el token de Unify
-var login = function(callback) {
-  User.findOne({ email: 'unify.argentina@gmail.com' })
+var login = function(email, password, callback) {
+  User.findOne({ email: email })
     .populate('main_circle')
     .exec(function(err, user) {
-    request(API_URL)
-      .post(LOGIN_PATH)
-      .send({
-        email: 'unify.argentina@gmail.com',
-        password: 'This is not my real password'
-      })
-      .end(function(err, data) {
-        callback(user, data.res.body.token);
-      });
-  });
+      request(API_URL)
+        .post(LOGIN_PATH)
+        .send({
+          email: email,
+          password: password
+        })
+        .end(function(err, data) {
+          callback(user, data.res.body.token);
+        });
+    });
 };
 
 describe('Users API', function() {
@@ -40,11 +41,15 @@ describe('Users API', function() {
   before(function(done) {
     mongoose.connect(config.MONGODB_TEST);
     User.remove().exec(function(err) {
-      User.create({
+      User.create([{
         name: 'Juan Losa',
         email: 'unify.argentina@gmail.com',
         password: 'This is not my real password'
-      }, done);
+      }, {
+        name: 'Juan Losa',
+        email: 'unify.argentina2@gmail.com',
+        password: 'This is not my real password'
+      }], done);
     });
   });
 
@@ -80,36 +85,22 @@ describe('Users API', function() {
 
     it('should not allow to get user information with other user id', function(done) {
       // Primero creamos un nuevo usuario
-      User.create({
-        name: 'Juan Losa',
-        email: 'unify.argentina2@gmail.com',
-        password: 'This is not my real password'
-      }, function(err, user) {
-        // Luego nos logueamos con el primer usuario, obteniendo el token de unify
-        request(API_URL)
-          .post(LOGIN_PATH)
-          .send({
-            email: 'unify.argentina@gmail.com',
-            password: 'This is not my real password'
-          })
-          // Por último, intentamos obtener la info del nuevo usuario con el token del primer usuario
-          .end(function(err, data) {
-            User.findOne({ email: 'unify.argentina@gmail.com' }, function(err, user2) {
-              request(API_URL)
-                .get(USERS_PATH + user._id)
-                .set('Authorization', 'Bearer ' + data.res.body.token)
-                .end(function(err, data) {
-                  data.res.statusCode.should.equal(400);
-                  data.res.body.errors[0].msg.should.equal('You are trying to find a different user');
-                  done();
-                });
+      login('unify.argentina@gmail.com', 'This is not my real password', function(user, token) {
+        User.findOne({ email: 'unify.argentina2@gmail.com' }, function(err, user2) {
+          request(API_URL)
+            .get(USERS_PATH + user2._id)
+            .set('Authorization', 'Bearer ' + token)
+            .end(function(err, data) {
+              data.res.statusCode.should.equal(400);
+              data.res.body.errors[0].msg.should.equal('You are trying to find a different user');
+              done();
             });
-          });
+        });
       });
     });
 
     it('should allow to get user information with valid access token', function(done) {
-      login(function(user, token) {
+      login('unify.argentina@gmail.com', 'This is not my real password', function(user, token) {
         request(API_URL)
           .get(USERS_PATH + user._id)
           .set('Authorization', 'Bearer ' + token)
@@ -140,7 +131,7 @@ describe('Users API', function() {
     });
 
     it('should not allow to update user information with empty data', function(done) {
-      login(function(user, token) {
+      login('unify.argentina@gmail.com', 'This is not my real password', function(user, token) {
         request(API_URL)
           .put(USERS_PATH + user._id)
           .set('Authorization', 'Bearer ' + token)
@@ -166,7 +157,7 @@ describe('Users API', function() {
     });
 
     it('should not allow to update user information with wrong data', function(done) {
-      login(function(user, token) {
+      login('unify.argentina@gmail.com', 'This is not my real password', function(user, token) {
         request(API_URL)
           .put(USERS_PATH + user._id)
           .set('Authorization', 'Bearer ' + token)
@@ -194,7 +185,7 @@ describe('Users API', function() {
     });
 
     it('should not allow to update user information with injection data', function(done) {
-      login(function(user, token) {
+      login('unify.argentina@gmail.com', 'This is not my real password', function(user, token) {
         request(API_URL)
           .put(USERS_PATH + user._id)
           .set('Authorization', 'Bearer ' + token)
@@ -215,7 +206,7 @@ describe('Users API', function() {
     // TODO AGREGAR TEST PROBANDO DE MODIFICAR EL MAIL POR UNO EXISTENTE
 
     it('should allow to update user information with unexistent email', function(done) {
-      login(function(user, token) {
+      login('unify.argentina@gmail.com', 'This is not my real password', function(user, token) {
         request(API_URL)
           .put(USERS_PATH + user._id)
           .set('Authorization', 'Bearer ' + token)
@@ -238,12 +229,12 @@ describe('Users API', function() {
     });
 
     it('should allow to update user information with existent email', function(done) {
-      login(function(user, token) {
+      login('unify.argentina2@gmail.com', 'This is not my real password', function(user, token) {
         request(API_URL)
           .put(USERS_PATH + user._id)
           .set('Authorization', 'Bearer ' + token)
           .send({
-            email: 'unify.argentina@gmail.com',
+            email: 'unify.argentina3@gmail.com',
             name: 'b',
             password: 'aaaaaa',
             confirm_password: 'aaaaaa'
@@ -252,7 +243,7 @@ describe('Users API', function() {
             data.res.statusCode.should.equal(200);
             var jsonUser = data.res.body.user;
             jsonUser._id.should.equal(user._id.toString());
-            jsonUser.email.should.equal('unify.argentina@gmail.com');
+            jsonUser.email.should.equal('unify.argentina3@gmail.com');
             jsonUser.name.should.equal('b');
             jsonUser.main_circle.name.should.equal(user.main_circle.name);
             done();
